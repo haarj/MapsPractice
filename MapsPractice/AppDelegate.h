8eff781d34d19eb8a45712554fbee27f6b8e55ac
Justin Haar <justin.haar22@gmail.com>
2015-03-26 16:12:36 -0700

    Initial Commit

---
+++ MapsPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapsPractice
+//
+//  Created by Justin Haar on 3/26/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
